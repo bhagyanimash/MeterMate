576bb29798179fe36c7b741b3d9325e044a1eca1
bhagyanimash <bhagyanimash2000@gmail.com>
2024-01-16 15:32:41 +0530

    Rebuild loading page

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:metermate/pages/loading_page.dart';
 
 void main() {
-  runApp(const Metermate());
+  runApp(const MeterMate());
 }
 
-class Metermate extends StatelessWidget {
-  const Metermate({super.key});
+class MeterMate extends StatelessWidget {
+  const MeterMate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'images/metermate.png',
-                width: 125,
-              ),
-              LoadingAnimationWidget.prograssiveDots(
-                  color: const Color.fromRGBO(3, 2, 64, 1.000), size: 70)
-            ],
-          ),
-        ),
-      ),
+      home: LoadingPage(),
     );
   }
 }
