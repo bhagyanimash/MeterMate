6eb21bb879df6aabbbf4eb6a8a41dde256dd9103
bhagyanimash <bhagyanimash2000@gmail.com>
2024-01-16 16:05:17 +0530

    changing app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:metermate/pages/loading_page.dart';
 
 void main() {
