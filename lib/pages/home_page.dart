a9104fde7909b02e094af74d770ecba093784d27
bhagyanimash <bhagyanimash2000@gmail.com>
2024-01-17 14:38:07 +0530

    create home page app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,58 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Row(
+            children: [
+              const Padding(padding: EdgeInsets.only(left: 15)),
+              Image.asset(
+                'images/onlylogo.png',
+                width: 40,
+              ),
+            ],
+          ),
+          title: const Text(
+            "MeterMate",
+            style: TextStyle(
+              color: Color.fromRGBO(
+                3,
+                2,
+                64,
+                1.000,
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+              iconSize: 30,
+              onPressed: () {},
+              icon: const Icon(
+                Icons.notifications,
+                color: Color.fromRGBO(
+                  3,
+                  2,
+                  64,
+                  1.000,
+                ),
+              ),
+            ),
+            IconButton(
+              iconSize: 30,
+              onPressed: () {},
+              icon: const Icon(
+                Icons.account_circle,
+                color: Color.fromRGBO(
+                  3,
+                  2,
+                  64,
+                  1.000,
+                ),
+              ),
+            ),
+          ]),
+    );
   }
 }
