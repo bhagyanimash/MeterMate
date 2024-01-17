b211e1b1bc41d91906ec2be37064dcf39c7826ba
C.D. Sandaruwan Senanayake <sdlcdssenanayake@std.appsc.sab.ac.lk>
2024-01-18 00:00:29 +0530

    create test scanner page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:metermate/pages/loading_page.dart';
+import 'package:metermate/pages/test_scanner_page.dart';
+
 
 void main() {
   runApp(const MeterMate());
@@ -13,7 +13,7 @@ class MeterMate extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoadingPage(),
+      home: TestScannerPage(),
     );
   }
 }
---
+++ lib/pages/test_scanner_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestScannerPage extends StatefulWidget {
+  const TestScannerPage({super.key});
+
+  @override
+  State<TestScannerPage> createState() => _ScannerPageState();
+}
+
+class _ScannerPageState extends State<TestScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
