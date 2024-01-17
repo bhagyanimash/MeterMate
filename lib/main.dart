954cd4fcdfe811b977b6dd2c4ea9125a127c1bb3
bhagyanimash <bhagyanimash2000@gmail.com>
2024-01-17 10:06:30 +0530

    creating login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:metermate/pages/loading_page.dart';
+import 'package:metermate/pages/login_page.dart';
 
 void main() {
   runApp(const MeterMate());
@@ -12,7 +13,7 @@ class MeterMate extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoadingPage(),
+      home: LoginPage(),
     );
   }
 }
