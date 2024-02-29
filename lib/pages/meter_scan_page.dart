879d466326e7f5ec9f311e50311f6b90d66310d5
bhagyanimash <bhagyanimash2000@gmail.com>
2024-02-29 21:07:53 +0530

    develop meter_scan_page

--- lib/pages/meter_scan_page.dart
+++ lib/pages/meter_scan_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MeterScanPage extends StatefulWidget {
   const MeterScanPage({super.key});
@@ -8,6 +10,49 @@ class MeterScanPage extends StatefulWidget {
 }
 
 class _MeterScanPageState extends State<MeterScanPage> {
+  String _text = '';
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final pickedImage = await _picker.pickImage(source: ImageSource.camera);
+    if (pickedImage != null) {
+      final inputImage = InputImage.fromFilePath(pickedImage.path);
+      final textDetector = GoogleMlKit.vision.textRecognizer();
+      final RecognizedText recognizedText =
+          await textDetector.processImage(inputImage);
+
+      // Initialize an empty buffer to collect numeric text.
+      final StringBuffer numericText = StringBuffer();
+
+      // Define a regex pattern to match numbers. This pattern matches whole numbers and decimals.
+      final RegExp numPattern = RegExp(r'\b\d+\.?\d*\b');
+
+      for (TextBlock block in recognizedText.blocks) {
+        for (TextLine line in block.lines) {
+          // For each line, check if it matches the numeric pattern.
+          // If it does, append it to the numericText buffer.
+          if (numPattern.hasMatch(line.text)) {
+            // This extracts and appends all numeric matches within the line.
+            final matches = numPattern.allMatches(line.text);
+            for (var match in matches) {
+              numericText.writeln(match.group(0));
+            }
+          }
+        }
+      }
+
+      // Update the state with the extracted numeric text.
+      setState(() {
+        _text = numericText.toString();
+      });
+
+      // Don't forget to close the detector when it's no longer needed.
+      textDetector.close();
+
+      // After extracting numbers, you can proceed with uploading the text to Firebase as needed.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +72,7 @@ class _MeterScanPageState extends State<MeterScanPage> {
             const SizedBox(
               height: 40,
             ),
+            if (_text.isNotEmpty) SelectableText(_text),
             SizedBox(
               height: 50,
               width: 350,
@@ -36,7 +82,7 @@ class _MeterScanPageState extends State<MeterScanPage> {
                 //padding: EdgeInsets.symmetric(),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20.0)),
-                onPressed: () {},
+                onPressed: _pickImage,
                 child: const Text(
                   "Capture Meter Board",
                   style: TextStyle(
